897be51b8243146d1a3b4b3e473180775e40b294
cpatrick2626 <cpatrick2626@yahoo.com>
2026-04-26 02:33:28 -0400

    Add files via upload

--- data/fetch_odds.py
+++ data/fetch_odds.py
@@ -1,1 +1,18 @@
+import requests
+from config import ODDS_API_KEY
+from utils.api_manager import fetch_with_cache
 
+def fetch_odds_api():
+    try:
+        url = "https://api.the-odds-api.com/v4/sports/icehockey_nhl/odds/"
+        params = {
+            "apiKey": ODDS_API_KEY,
+            "regions": "us",
+            "markets": "totals"
+        }
+        return requests.get(url, params=params).json()
+    except:
+        return []
+
+def get_odds():
+    return fetch_with_cache("odds", fetch_odds_api, 300)
