e15308405537999109c7d418a61f37f53de6de0d
cpatrick2626 <cpatrick2626@yahoo.com>
2026-04-26 05:11:52 -0400

    Update fetch_odds.py

--- data/fetch_odds.py
+++ data/fetch_odds.py
@@ -1,10 +1,8 @@
 from data.api_manager import get_odds as api_get_odds
 
-
 def get_odds(force_refresh=False):
     data, usage = api_get_odds(force_refresh=force_refresh)
 
-    # ensure safe return
     if not isinstance(data, list):
         data = []
 
